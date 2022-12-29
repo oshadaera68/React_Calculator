ebfcd203b40acc3aa0efbe97d5ea45f7ec07ccff
oshadaera68 <mindartlk010@gmail.com>
2022-12-29 23:08:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-oprand"></div>
-        <div className="current-oprand"></div>
+        <div className="previous-oprand">hello</div>
+        <div className="current-oprand">hello1</div>
       </div>
     </div>
   );
