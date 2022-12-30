23e462c05d2af24f498fe6adc0bd16e7c516d028
oshadaera68 <mindartlk010@gmail.com>
2022-12-30 11:11:03 +0530

    updated content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,87 @@
+
+
 import React from 'react';
+import './app.css';
+import { useReducer } from 'react';
+import DigitButton from './DigitButton';
+import OperationButton from './OperationButton';
+
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      if (payload.digit === "0" && state.currentOperand === "0") {
+        return state
+      }
+
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
+        return state
+      }
+
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`
+      }
+    
+    case ACTIONS.CHOOSE_OPERATION:
+      if(state.currentOperand == null && state.previousOperand === null) { 
+        return state
+      }
+
+      if(state.previousOperand==null){
+        return{
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand:null,
+        }
+      }
+
+
+    case ACTIONS.CLEAR:
+      return {}
+    default:
+      break;
+  }
+}
 
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {});
+
+  //dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } })
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-oprand">hello</div>
-        <div className="current-oprand">hello1</div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
+      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
+      <button>DEL</button>
+      <OperationButton operation="÷" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
+      <button className='span-two'>=</button>
     </div>
   );
 }
---
+++ src/OperationButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ACTIONS } from './App'
+
+function OperationButton({ dispatch, operation }) {
+    return (
+        <button 
+        onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}
+        >
+            {operation}</button>)
+}
+
+export default OperationButton;
